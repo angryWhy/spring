12e20f26f911fb18f91f4f1ab136fba16350ef81
angryWhy <875978822@qq.com>
2023-03-08 09:16:51 +0800

    spring

--- src/com/component/UserDao.java
+++ src/com/component/UserDao.java
@@ -2,7 +2,7 @@ package src.com.component;
 
 import org.springframework.stereotype.Repository;
 //@Repository标识该类是一个repository，持久化层的一个类
-@Repository
+@Repository(value = "thisId")
 public class UserDao {
 
 }
